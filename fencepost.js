d4aded672d09bb92cfcb2b9f406e53b99972da39
Paul E Lederer <plederer93@gmail.com>
2017-02-22 15:21:04 -0600

    first commit

---
+++ fencepost.js
@@ -0,0 +1,15 @@
+/****************************************************************************
+ * Write a function that takes a positive integer and returns a string with *
+ * every number from 1 to that integer fenced in with vertical lines.       *
+ * Make sure to validate our data in case our user is a neanderthal.        *
+ * Example: fence(5) should return "|1|2|3|4|5|"                            *
+ * @method fence                                                            *
+ * @param  {Integer} times [A positive Integer]                             *
+ * @return {String}       [fenced in numbers]                               *
+ ****************************************************************************/
+
+
+function fence(times)
+{
+
+}
---
+++ prime.js
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * Write a function that takes a positive integer and returns all prime       *
+ * numbers from 1 to that integer fenced in with vertical lines.              *
+ * Make sure to validate our data in case our user is a neanderthal.          *
+ * Example: fence(10) should return [1,2,3,5,7]                               *
+ * Use the Sieve of Eratosthenes                                              *
+ * https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes                        *
+ * @method prime                                                              *
+ * @param  {Integer} limit [A Positive integer]                               *
+ * @return {Array}         [prime numbers]                                    *
+ ******************************************************************************/
+
+function prime(limit)
+{
+
+}
