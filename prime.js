c87f289bf578066912317dc48da29dea695abc7d
Paul E Lederer <plederer93@gmail.com>
2017-02-23 10:48:58 -0600

    fixed a typo

--- prime.js
+++ prime.js
@@ -2,7 +2,7 @@
    * Write a function that takes a positive integer and returns all prime     *
    * numbers from 1 to that integer in an array                               *
    * Make sure to validate our data in case our user is a neanderthal.        *
-   * Example: fence(10) should return [1,2,3,5,7]                             *
+   * Example: prime(10) should return [1,2,3,5,7]                             *
    * Use the Sieve of Eratosthenes                                            *
    * https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes                      *
    * @method prime                                                            *
